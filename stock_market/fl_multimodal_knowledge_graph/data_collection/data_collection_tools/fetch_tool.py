e004d6c1778bd05a41efd3a74eddb2bf969bd8d1
laran <qanhnguyen811@yahoo.com>
2024-07-24 15:57:22 +1000

    - add transform_path - add get_page - add get_node - add combine_pages - add get_edge - add get_multi_model

--- stock_market/fl_multimodal_knowledge_graph/data_collection/data_collection_tools/fetch_tool.py
+++ stock_market/fl_multimodal_knowledge_graph/data_collection/data_collection_tools/fetch_tool.py
@@ -60,3 +60,103 @@ def transform_filename(uri):
             uri = blocks[0] + blocks[1].replace('/','@') + blocks[2] #Replacing slashes within the brackets and rearranging the URI
     return uri
 
+def transform_path(uri):
+    '''Takes a URI and transforms it into a directory structure.
+    Inputs:
+    uri(string): A string of URI.
+    Returns:
+    string: a directory structure.
+    '''
+    element_type = uri[1]
+    if element_type == 'c': #If the element is a node
+        path = re.findall('^(.*)\[.*?',uri)[0] #Using regular expression to find and only retain the first part of the file from the URI
+    else: #If the element is an edge
+        path = re.findall('^/(.*)/\[.*?\]',uri)[0] #Using regular expression to find and remove the last level of the filename contained within the bracket
+
+    return path
+
+def get_page(uri):
+    '''Takes a URI and returns a page. If the page is already saved locally, it reads the page from the local file.
+    Otherwise,it downloads the page and indicates that the page needs to be saved.
+    Inputs:
+    uri(string): A string of a URI.
+    Returns:
+    tuple: A tuple containing the page data and a boolean indicating whether the page needs to be saved.
+    '''
+    need_to_save = False
+    filename = transform_filename(uri)
+    try: #Attempting a block of code before throwing an exception
+        with open(filename, 'r') as f: #Opening a file in a read mode, automatically closing the file when done reading (with)
+            page = json.loads(f) #parsing a JSON formatted string into a Python object
+    except FileNotFoundError: #Throwing an exception error if FileNotFoundError
+        page = download(uri) #Downloading the page data
+        need_to_save = True
+    except json.JSONDecodeError: #Throwing an exception by json module when there is an error with JSON decoding a file.
+        print(f"Error: {filename} does not contain valid JSON.")
+        page = None
+    return page, need_to_save
+
+def get_node(uri):
+    '''Takes a URI and returns a node. If the node is paginated, the function fetches all the pages and combines them into a single node.
+    Inputs:
+    uri(string): A string of a URI.
+    Returns:
+    tuple: A tuple containing the combined node data and a boolean indicating whether the node was fetched from a web
+    '''
+    node,is_from_web = get_page(uri) #Returning node and whether it is from the web.
+    if is_from_web and 'view' in node: #If the node was fetched from the web and is paginated, combine all the pages
+        node = combine_pages(node)
+        return node, is_from_web
+def combine_pages(node):
+    ''' Takes all the pages of a paginated node and combines them into a single node.
+    Inputs:
+    node(dictionary): A dictionary containing the initial page of the node.
+    Returns:
+    dictionary: A dictionary containing the combined node data
+    '''
+    while 'view' in node and 'nextPage' in node['view']:#Checking two conditions: whether the 'view' key exists in the 'node' dictionary and if the first condition is true, it checks if 'nextPage' is a key in the dictionary that the node['view'] points to.
+        try:  # Attempting a block of code before throwing an exception
+            next_page = download(node['view']['nextPage'])
+        except Exception as e: #Throwing an Exception error as a variable e
+            print(f"An error occurred while downloading the next page:{e}")
+            break #Stopping at this error
+        node['edges'] += next_page['edges'] #Adding the edge of 'nextPage' to the existing edges of the node
+    del node['view'] #Removing the 'view' key as we no longer need it
+    return node
+
+def get_edge(uri):
+    '''Takes a URI and returns the edge.
+    Inputs:
+    uri(string): A string of a URI.
+    Returns:
+    tuple or None: A tuple containing the page data and a boolean indicating whether we need to save the edge locally if can be fetched, None otherwise.
+    '''
+    try:# Attempting a block of code before throwing an exception
+        return get_page(uri)
+    except Exception as e:
+        print(f"An error occurred while fetching the edge data:{e}")
+        return None
+def get_multi_model(info):
+    '''Takes information of an entity and return the content of the file.
+    Inputs:
+    info(dictionary): A string of the information of an entity.
+    Returns:
+    bytes or None: The content of the file as bytes if it could be read, or None otherwise.
+    '''
+    filename = transform_filename(info['@id']) #Transform the id of the information with into a complete file path
+    filename = filename + '.' + info['form'] #Adding '.' and form of the information to the filename
+    try: # Attempting a block of code before throwing an exception
+        with open(filename, 'rb') as f: #Openning the file in a binary mode for reading and closing it when done
+    except FileNotFoundError: ##Throwing an exception error if FileNotFoundError
+        print(f"Error:The file {filename} does not exist.")
+        content = None
+    except IOError as e: #Throwing an IOError exception as a variable e
+        print(f"Error:An I/O error occurred while reading the file {filename}: {e}")
+        content = None
+        return content
+
+
+
+
+
+
